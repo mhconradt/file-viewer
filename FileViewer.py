328a8a697d783dd56263bb134355f969ba9a6370
mhconradt <mhconradt@protonmail.com>
2024-09-25 01:38:33 -0700

    File?

--- FileViewer.py
+++ FileViewer.py
@@ -133,6 +133,8 @@ def process_directory(directory):
     # Convert embeddings to numpy array
     embedding_array = np.array(embeddings_list)
 
+    print(locals())
+
     # Reduce embeddings to 3D using UMAP
     reducer = umap.UMAP(n_components=3, random_state=42)
     embedding_3d = reducer.fit_transform(embedding_array)
